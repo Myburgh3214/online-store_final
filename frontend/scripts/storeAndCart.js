f9ec5fe7863a2d848581412eecc9744a70b743eb
Myburgh3214 <79691187+Myburgh3214@users.noreply.github.com>
2021-11-29 21:12:04 +0200

    finished front page with featured and upcoming products

--- frontend/scripts/storeAndCart.js
+++ frontend/scripts/storeAndCart.js
@@ -1,28 +1,82 @@
+window.productsReady = (products) => {
+  let productAttr = document.getElementById('products-list').getAttribute('patt') || null;
+  let singleProduct = false;
+  if (window.location.hash !== '' && window.location.hash !== '#') {
+    singleProduct = window.location.hash.substring(1);
+  }
+  let newHtml = '';
+  for (let product of products) {
+    if (singleProduct !== false) {
+      if (singleProduct !== product.id) continue;
+      singleProduct = product;
+    } else if (productAttr !== null) {
+      if (product[productAttr] != "1") continue;
+    }
+
+    newHtml += `<div class="card ${singleProduct !== false ? 'enlarged' : ''}"><div class="av-center">
+      <div class="av-prod-img ${(singleProduct === false ? 'av-sm' : '')}" style="background-image: url('images/${product.image}');"></div>
+        </div><p class="product-name" >${product.name}</p>
+        <p class="product-description">${product.description}</p>
+        <p class="product-price" >R${product.price}</p>`;
+    if (singleProduct === false)
+      newHtml +=`<button onclick="window.goToProduct('${product.id}')">VIEW ITEM</button>`
+    newHtml += `<button onclick="addProductToCart('${product.id}', '${product.name}')" >ADD TO CART</button>
+      </div>`;
+  }
+  document.getElementById('products-list').innerHTML = newHtml;
+  if (singleProduct !== false) {
+    document.getElementsByTagName('title')[0].innerText = `${singleProduct.name} - Online Store`;
+  }
+};
+function addProductToCart(productId, name) {
+  window.cart.addItemToCart(productId);
+  window.showCart();
+}
+
+// handle cart code
+function placeOrder() {
+  window.cart.placeOrder(result => {
+    if (result.status === 401) { // login required
+      alert("Please login/sign up before placing your order...");
+      location.href='logIn.html';
+      return;
+    }
+    if (result.status === 201) { // login required
+      alert("Thanks for placing your order with us :)");
+      window.cart.clearCart();
+      return;
+    }
+    result.text().then(console.log)
+  });
+}
+function cartItemQtyChangeEvent (e) {
+  let qty = Number.parseInt(e.value);
+  if (qty < 1 || isNaN(qty))
+    qty = 1;
+  window.cart.updateOrAddItemToCart(e.getAttribute('productId'), qty);
+}
+window.cartUpdated = (newCart) => {
+  let newHtml = '';
+
+  for (let cartItem of newCart.getItems()) {
+    let product = cartItem.getProduct();
+    let sneakyId = `cart-product-qty-field-${product.id}`;
+    newHtml += `<tr><td>${product.name}</td><td>
+      <input type="number" value="${cartItem.getQty()}" style="width: 40px;" productId="${product.id}" onchange="cartItemQtyChangeEvent(this)" />
+      <a href="javascript:window.cart.removeItemFromCart('${product.id}');">x</a>
+      </td><td>R${cartItem.priceTotal().toFixed(2)}</td></tr>`;
+  }
+
+  document.getElementById('clean-cart-body').innerHTML = newHtml;
+  document.getElementById('clean-cart-total').innerText = `R${newCart.getTotal().toFixed(2)}`;
+}
+
 window.showCart = () =>{  
   document.getElementById('clean-cart').classList.remove('hidden');
 }
 window.hideCart = () =>{  
   document.getElementById('clean-cart').classList.add('hidden');
 }
-
-getRequest("products", (responseAsObj) => { // Get the current session information from the API for the current browser session
-  responseAsObj.json().then(response => { // parse the response into JSON
-    for (let i = 0 ; i < response.length ; i++) {
-      response[i].price = Number.parseFloat(response[i].price);
-    }
-    if (window.productsReady !== undefined) {
-      window.productsReady(response); // if the productsReady function is on window, it will be called to let us know we can load the products on screen
-    }
-    window.goToProduct = (id) => {
-      window.location.hash = `#${id}`;
-      window.location.reload();
-    }
-    window.products = response;
-    window.cart = new cart(window.sessionId);
-
-    window.hideCart();
-  });
-});
 
 function cart(orderId) { // we match the session id to the order id ....
   this._id = orderId;
@@ -173,4 +227,25 @@ function cartItem(productId, qty) {
   }
 }
 
-                                                                                                                                                                               
+
+getRequest("products", (responseAsObj) => { // Get the current session information from the API for the current browser session
+  responseAsObj.json().then(response => { // parse the response into JSON
+    for (let i = 0 ; i < response.length ; i++) {
+      response[i].price = Number.parseFloat(response[i].price);
+    }
+    if (window.productsReady !== undefined) {
+      window.productsReady(response); // if the productsReady function is on window, it will be called to let us know we can load the products on screen
+    }
+    window.goToProduct = (id) => {
+      if (location.pathname.indexOf('store.html') >= 0) {
+        window.location.hash = `#${id}`;
+        window.location.reload();
+      } else  
+        location.href=`store.html#${id}`;
+    }
+    window.products = response;
+    window.cart = new cart(window.sessionId);
+
+    window.hideCart();
+  });
+});
